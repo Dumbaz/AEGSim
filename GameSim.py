30ead45f8b972de9abaa9ca84f1b59f4ce6034f2
Dumbaz <iwantmyfingerback+windowsgithub@gmail.com>
2016-01-14 13:59:40 +0100

    Most buildings with their basic logic

--- GameSim.py
+++ GameSim.py
@@ -1,54 +1,173 @@
+subscibers = []
+
+def notify(time):
+	for subsciber in subscibers:
+		subsciber.update(time)
+
 class Game(object):
-	"""docstring for Game"""
+	"""docstring for Game
+		Gametime is in seconds
+		runGame() runs the game until 20 Minutes have passed
+	"""
 	def __init__(self, gametime):
 		super(Game, self).__init__()
 		self.gametime = gametime
 		self.spieler = []
-
 	def __repr__(self):
 		return 'In this Game, there are %s, the current gametime is %s' % (self.spieler, self.gametime)
-		
+	def updatetimeSeconds(self, count):
+		notify(count)
+		self.gametime += count
+		return self.gametime
+	def updatetimeMinutes(self, count):
+		notify(count*60)
+		self.gametime += count*60
+		return self.gametime
+	def runGame(self):
+		while self.gametime < 1200:
+			notify(1)
+			self.gametime += 1
 
-class Basis(object):
+class Knotenpunkt(object):
+	"""docstring for Knotenpunkt"""
+	def __init__(self):
+		super(Knotenpunkt, self).__init__()
+		self.health = 100
+		self.leistung = 0
+		self.modulplaetze = 0
+		self.netzwerkkarten = 0
+		subscibers.append(self)
+	def __repr__(self):
+		return 'Dieser Knotenpunkt hat %s Lebenspunkte und %s Leistungspunkte.' % (self.health, self.leistung)
+	def changeHealth(self, count):
+		self.health += count
+		if self.health < 0:
+			self.health = 0
+	def changeLeistung(self, count):
+		self.leistung += count
+		if self.leistung < 0:
+			self.leistung = 0
+	def update(self, time):
+		if self.leistung == 0:
+			self.changeHealth(-(time*100))
+		elif self.leistung > 0:
+			self.changeHealth(time*50)
+
+class KleinerKnotenpunkt(Knotenpunkt):
+	"""docstring for KleinerKnotenpunkt"""
+	def __init__(self):
+		super(KleinerKnotenpunkt, self).__init__()
+		self.health = 3000
+		self.leistung = 5000
+		self.modulplaetze = 3
+		self.netzwerkkarten = 2
+
+class MittlererKnotenpunkt(Knotenpunkt):
+	"""docstring for MittlererKnotenpunkt"""
+	def __init__(self):
+		super(MittlererKnotenpunkt, self).__init__()
+		self.health = 4500
+		self.leistung = 7500
+		self.modulplaetze = 5
+		self.netzwerkkarten = 3
+
+class GrosserKnotenpunkt(Knotenpunkt):
+	"""docstring for GrosserKnotenpunkt"""
+	def __init__(self):
+		super(GrosserKnotenpunkt, self).__init__()
+		self.health = 6000
+		self.leistung = 10000
+		self.modulplaetze = 7
+		self.netzwerkkarten = 4
+
+class Basis(Knotenpunkt):
 	"""docstring for Basis"""
-	def __init__(self, health):
+	def __init__(self):
 		super(Basis, self).__init__()
-		self.health = health
-	def reduceHealth(self, count):
-		self.health -= count
+		self.health = 100000
+		self.Leistungspunkte = 50000
+		self.modulplaetze = 3
+		self.netzwerkkarten = 3
+		self.grundproduktion = 100
+		self.owner = None
+	def setOwner(self, player):
+		self.owner = player
+	def update(self, time):
+		self.owner.bitcoin += time * self.grundproduktion
+
+
+class Controlserver(object):
+	"""docstring for Controlserver"""
+	def __init__(self):
+		super(Controlserver, self).__init__()
+		self.kosten = 20000
+		self.leistungskosten = 1000
+		self.ownedclients = 0
+		self.clientrate = 2
+		self.owner = None
+		subscibers.append(self)
+	def setOwner(self, player):
+		self.owner = player
+	def update(self, time):
+		self.ownedclients += 2 * time
+
+class Bitcoinmine(object):
+	"""docstring for Bitcoinmine"""
+	def __init__(self):
+		super(Bitcoinmine, self).__init__()
+		self.kosten = 10000
+		self.produktion = 350
+		self.leistungskosten = 500
+		self.owner = None
+		subscibers.append(self)
+	def setOwner(self, player):
+		self.owner = player
+	def update(self, time):
+		self.owner.bitcoin += time * self.produktion
+
 		
 class Spieler(object):
 	"""docstring for Spieler"""
-	def __init__(self, name, startgeld, bandbreite):
+	def __init__(self, name, startgeld, bandbreite, game, basis):
 		super(Spieler, self).__init__()
 		self.name = name
 		self.bitcoin = startgeld
 		self.bandbreite = bandbreite
-
+		self.basis = basis
+		game.spieler.append(self)
 	def __repr__(self):
 		return 'Spieler %s has Bitcoin: %s, Bandbreite: %s' % (self.name, self.bitcoin, self.bandbreite)
-		
 
-"""Game is supposed to last for 20 Minutes"""
 
-def update(newgametime):
-	Game.gametime = newgametime
+def setupGame(game):
+	spieler1basis = Basis()
+	hans = Spieler('Hans', 30000, 6000, game, spieler1basis)
+	spieler1basis.setOwner(hans)
 
-def main():
-	game = Game(0)
-	hans = Spieler('Hans', 3000, 6000)
-	peter = Spieler('Peter', 3000, 6000)
-	game.spieler.append(hans)
-	game.spieler.append(peter)
+	spieler2basis = Basis()
+	peter = Spieler('Peter', 30000, 6000, game, spieler2basis)
+	spieler2basis.setOwner(peter)
+
 
 	print(game)
 
-	basiseins = Basis(100)
-	basiszwei = Basis(100)
-	print(basiseins.health)
-	print(basiszwei.health)
-	basiseins.reduceHealth(50)
-	print(basiseins.health)
+"""Game is supposed to last for 20 Minutes"""
+
+def main():
+	game = Game(0)
+	setupGame(game)
+
+	testbase = Knotenpunkt()
+	testbase.changeLeistung(-120)
+	testbase.changeLeistung(10)
+
+
+	game.updatetimeSeconds(50)
+
+	game.updatetimeMinutes(15)
+
+	game.runGame()
+	print(game)
 
 if __name__ == '__main__':
 	main()
