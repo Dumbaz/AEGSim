925e5e0b4dd8ae6eea0187338c941686dc619983
Dumbaz <iwantmyfingerback+windowsgithub@gmail.com>
2016-01-14 14:08:38 +0100

    Split project into modules
    Updated .gitignore

--- GameSim.py
+++ GameSim.py
@@ -1,3 +1,6 @@
+import buildings
+
+
 subscibers = []
 
 def notify(time):
@@ -28,102 +31,7 @@ class Game(object):
 			notify(1)
 			self.gametime += 1
 
-class Knotenpunkt(object):
-	"""docstring for Knotenpunkt"""
-	def __init__(self):
-		super(Knotenpunkt, self).__init__()
-		self.health = 100
-		self.leistung = 0
-		self.modulplaetze = 0
-		self.netzwerkkarten = 0
-		subscibers.append(self)
-	def __repr__(self):
-		return 'Dieser Knotenpunkt hat %s Lebenspunkte und %s Leistungspunkte.' % (self.health, self.leistung)
-	def changeHealth(self, count):
-		self.health += count
-		if self.health < 0:
-			self.health = 0
-	def changeLeistung(self, count):
-		self.leistung += count
-		if self.leistung < 0:
-			self.leistung = 0
-	def update(self, time):
-		if self.leistung == 0:
-			self.changeHealth(-(time*100))
-		elif self.leistung > 0:
-			self.changeHealth(time*50)
 
-class KleinerKnotenpunkt(Knotenpunkt):
-	"""docstring for KleinerKnotenpunkt"""
-	def __init__(self):
-		super(KleinerKnotenpunkt, self).__init__()
-		self.health = 3000
-		self.leistung = 5000
-		self.modulplaetze = 3
-		self.netzwerkkarten = 2
-
-class MittlererKnotenpunkt(Knotenpunkt):
-	"""docstring for MittlererKnotenpunkt"""
-	def __init__(self):
-		super(MittlererKnotenpunkt, self).__init__()
-		self.health = 4500
-		self.leistung = 7500
-		self.modulplaetze = 5
-		self.netzwerkkarten = 3
-
-class GrosserKnotenpunkt(Knotenpunkt):
-	"""docstring for GrosserKnotenpunkt"""
-	def __init__(self):
-		super(GrosserKnotenpunkt, self).__init__()
-		self.health = 6000
-		self.leistung = 10000
-		self.modulplaetze = 7
-		self.netzwerkkarten = 4
-
-class Basis(Knotenpunkt):
-	"""docstring for Basis"""
-	def __init__(self):
-		super(Basis, self).__init__()
-		self.health = 100000
-		self.Leistungspunkte = 50000
-		self.modulplaetze = 3
-		self.netzwerkkarten = 3
-		self.grundproduktion = 100
-		self.owner = None
-	def setOwner(self, player):
-		self.owner = player
-	def update(self, time):
-		self.owner.bitcoin += time * self.grundproduktion
-
-
-class Controlserver(object):
-	"""docstring for Controlserver"""
-	def __init__(self):
-		super(Controlserver, self).__init__()
-		self.kosten = 20000
-		self.leistungskosten = 1000
-		self.ownedclients = 0
-		self.clientrate = 2
-		self.owner = None
-		subscibers.append(self)
-	def setOwner(self, player):
-		self.owner = player
-	def update(self, time):
-		self.ownedclients += 2 * time
-
-class Bitcoinmine(object):
-	"""docstring for Bitcoinmine"""
-	def __init__(self):
-		super(Bitcoinmine, self).__init__()
-		self.kosten = 10000
-		self.produktion = 350
-		self.leistungskosten = 500
-		self.owner = None
-		subscibers.append(self)
-	def setOwner(self, player):
-		self.owner = player
-	def update(self, time):
-		self.owner.bitcoin += time * self.produktion
 
 		
 class Spieler(object):
@@ -140,11 +48,11 @@ class Spieler(object):
 
 
 def setupGame(game):
-	spieler1basis = Basis()
+	spieler1basis = buildings.Basis()
 	hans = Spieler('Hans', 30000, 6000, game, spieler1basis)
 	spieler1basis.setOwner(hans)
 
-	spieler2basis = Basis()
+	spieler2basis = buildings.Basis()
 	peter = Spieler('Peter', 30000, 6000, game, spieler2basis)
 	spieler2basis.setOwner(peter)
 
@@ -157,7 +65,7 @@ def main():
 	game = Game(0)
 	setupGame(game)
 
-	testbase = Knotenpunkt()
+	testbase = buildings.Knotenpunkt()
 	testbase.changeLeistung(-120)
 	testbase.changeLeistung(10)
 
---
+++ buildings.py
@@ -0,0 +1,98 @@
+import GameSim
+
+class Knotenpunkt(object):
+	"""docstring for Knotenpunkt"""
+	def __init__(self):
+		super(Knotenpunkt, self).__init__()
+		self.health = 100
+		self.leistung = 0
+		self.modulplaetze = 0
+		self.netzwerkkarten = 0
+		GameSim.subscibers.append(self)
+	def __repr__(self):
+		return 'Dieser Knotenpunkt hat %s Lebenspunkte und %s Leistungspunkte.' % (self.health, self.leistung)
+	def changeHealth(self, count):
+		self.health += count
+		if self.health < 0:
+			self.health = 0
+	def changeLeistung(self, count):
+		self.leistung += count
+		if self.leistung < 0:
+			self.leistung = 0
+	def update(self, time):
+		if self.leistung == 0:
+			self.changeHealth(-(time*100))
+		elif self.leistung > 0:
+			self.changeHealth(time*50)
+
+class KleinerKnotenpunkt(Knotenpunkt):
+	"""docstring for KleinerKnotenpunkt"""
+	def __init__(self):
+		super(KleinerKnotenpunkt, self).__init__()
+		self.health = 3000
+		self.leistung = 5000
+		self.modulplaetze = 3
+		self.netzwerkkarten = 2
+
+class MittlererKnotenpunkt(Knotenpunkt):
+	"""docstring for MittlererKnotenpunkt"""
+	def __init__(self):
+		super(MittlererKnotenpunkt, self).__init__()
+		self.health = 4500
+		self.leistung = 7500
+		self.modulplaetze = 5
+		self.netzwerkkarten = 3
+
+class GrosserKnotenpunkt(Knotenpunkt):
+	"""docstring for GrosserKnotenpunkt"""
+	def __init__(self):
+		super(GrosserKnotenpunkt, self).__init__()
+		self.health = 6000
+		self.leistung = 10000
+		self.modulplaetze = 7
+		self.netzwerkkarten = 4
+
+class Basis(Knotenpunkt):
+	"""docstring for Basis"""
+	def __init__(self):
+		super(Basis, self).__init__()
+		self.health = 100000
+		self.Leistungspunkte = 50000
+		self.modulplaetze = 3
+		self.netzwerkkarten = 3
+		self.grundproduktion = 100
+		self.owner = None
+	def setOwner(self, player):
+		self.owner = player
+	def update(self, time):
+		self.owner.bitcoin += time * self.grundproduktion
+
+
+class Controlserver(object):
+	"""docstring for Controlserver"""
+	def __init__(self):
+		super(Controlserver, self).__init__()
+		self.kosten = 20000
+		self.leistungskosten = 1000
+		self.ownedclients = 0
+		self.clientrate = 2
+		self.owner = None
+		subscibers.append(self)
+	def setOwner(self, player):
+		self.owner = player
+	def update(self, time):
+		self.ownedclients += 2 * time
+
+class Bitcoinmine(object):
+	"""docstring for Bitcoinmine"""
+	def __init__(self):
+		super(Bitcoinmine, self).__init__()
+		self.kosten = 10000
+		self.produktion = 350
+		self.leistungskosten = 500
+		self.owner = None
+		subscibers.append(self)
+	def setOwner(self, player):
+		self.owner = player
+	def update(self, time):
+		self.owner.bitcoin += time * self.produktion
